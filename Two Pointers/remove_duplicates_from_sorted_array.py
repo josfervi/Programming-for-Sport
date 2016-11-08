6adf4c15f285a4e63dc4634a1bf55190093f0578
josfervi <jfv33@cornell.edu>
2016-11-08 16:03:48 +0000

    A[:]= A[:len_A] mutates fcn input A (side effect) whereas A= A[:len_A] does not

--- Two Pointers/remove_duplicates_from_sorted_array.py
+++ Two Pointers/remove_duplicates_from_sorted_array.py
@@ -4,10 +4,40 @@ class Solution:
     # @param A : list of integers
     # @return an integer
     
-    # - O(n) time complexity, where n = len(A)
+    # much more efficient: doesn't do: A[i +1:]= A[j:]
+    def removeDuplicates(self, A):
+        
+        i= 0
+        j= 0
+        len_A= len(A)
+        while i<len(A):
+            
+            A[j]= A[i]
+            j+= 1
+            i+= 1
+            
+            if i<len(A) and A[i-1] == A[i]:
+                
+                # skip over all duplicates
+                i+= 1
+                len_A-= 1
+                while i<len(A) and A[i-1] == A[i]:
+                    i+= 1
+                    len_A-= 1
+        
+        A[:]= A[:len_A] # modify A
+        return len_A
+
+# sol= Solution()
+# a= [ 0, 0, 0, 1, 1, 2, 2, 3 ]
+# print a
+# len_A= sol.removeDuplicates(a)
+# print a
+    
+    # - O(n) time complexity, where n = len(A) if we ignore the true cost of A[i +1:]= A[j:]
     # - O(1) extra space
     # - in place
-    def removeDuplicates(self, A):
+    def removeDuplicates_1(self, A):
         
         i= 0
         
