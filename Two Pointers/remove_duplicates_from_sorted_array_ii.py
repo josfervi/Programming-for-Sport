5775587f5fbfcbaabdfb0a4b85f4d598f64e2e61
josfervi <jfv33@cornell.edu>
2016-11-08 16:10:17 +0000

    A[j]= A[i] is way more efficient than A[j:]= A[i:]

--- Two Pointers/remove_duplicates_from_sorted_array_ii.py
+++ Two Pointers/remove_duplicates_from_sorted_array_ii.py
@@ -3,7 +3,36 @@
 class Solution:
     # @param A : list of integers
     # @return an integer
+    
+    # much more efficient: doesn't do: A[i+2:]= A[j:]
     def removeDuplicates(self, A):
+        
+        i= 0
+        j= 0
+        len_A= len(A)
+        while i<len(A):
+            
+            A[j]= A[i]
+            j+= 1
+            i+= 1
+            
+            if i<len(A) and A[i-1] == A[i]:
+                
+                A[j]= A[i]
+                j+= 1
+                i+= 1
+                
+                # skip over all duplicates
+                while i<len(A) and A[i-1] == A[i]:
+                    i+= 1
+                    len_A-= 1
+        
+        A[:]= A[:len_A]
+        
+        return len_A
+    
+    # does A[i+2:]= A[j:], which is not very efficient
+    def removeDuplicates_1(self, A):
         
         i= 0
         while i < len(A) -1: # { A[i-1] != A[i] for i>0 }
