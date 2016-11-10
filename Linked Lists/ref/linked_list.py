a13e5213701556d28acb9b4291f70c354f80671a
josfervi <jfv33@cornell.edu>
2016-11-10 01:29:47 +0000

    ?

--- Linked Lists/ref/linked_list.py
+++ Linked Lists/ref/linked_list.py
@@ -6,7 +6,9 @@ class ListNode(object):
     def __str__(self):
         return "%s --> %s" % (self.val, self.next)
 
-# a3= ListNode(3)
+# a5= ListNode(5)
+# a4= ListNode(4, a5 )
+# a3= ListNode(3, a4 )
 # a2= ListNode(2, a3 )
-# A=  ListNode(1,  a2)
-# A= ListNode(1, ListNode(2, ListNode(3) ) )
+# A=  ListNode(1, a2 )
+# A= ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5) ) ) ) )
