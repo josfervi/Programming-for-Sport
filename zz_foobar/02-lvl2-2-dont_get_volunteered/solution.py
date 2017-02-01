fcae504518e37a3bad34e935067c7e281fe3335d
josfervi <jfv33@cornell.edu>
2017-02-01 06:24:47 +0000

    use deque - a generalization of the queue and stack

--- zz_foobar/02-lvl2-2-dont_get_volunteered/solution.py
+++ zz_foobar/02-lvl2-2-dont_get_volunteered/solution.py
@@ -1,9 +1,11 @@
+# Submission: SUCCESSFUL. Completed in: 3 hrs, 51 mins, 53 secs.
+
 NUM_ROWS = 8
 NUM_COLS = 8
 
-# up-left indicates the move that is 2 squares up and 1 square to left
-# likewise left-up indicates the move that is 2 squares left and 1 square up
-# ans so on...
+# up-left indicates the move that is 2 squares up and 1 square to left.
+# Likewise, left-up indicates the move that is 2 squares left and 1 square up
+# And so on...
 
 MOVE_DELTAS = [ 
                 (-1, -2), # left-up
@@ -16,7 +18,63 @@ MOVE_DELTAS = [
                 (+1, -2), # left-down
                           ]
 
-# NUM_ROWS and NUM_COLS must be defined in global scope
+from collections import deque
+
+''' PRECONDITION: src and dst are each in range(0, NUM_ROWS*NUM_COLS) '''
+def answer(src, dst):
+    
+    if src == dst:
+        return 0
+    
+    minNumMoves = Board(NUM_ROWS, NUM_COLS, None)
+    # For square sq with row == r and col == c,
+    #   minNumMoves[sq] == minNumMoves.board[r][c] == the min num of moves to get
+    #   from the source square, srcSquare,
+    #   to sq the square with coords (r, c) if that value is known.
+    #
+    # While that value is unknown, minNumMoves[sq] == None.
+    
+    srcSquare = Square.fromNum(src, board = minNumMoves)
+    dstSquare = Square.fromNum(dst, board = minNumMoves)
+    
+    minNumMoves[srcSquare] = 0
+    
+    squares = deque([srcSquare]) # squares queue, notice that by the time a square, sq, is added to squares, minNumMoves[sq] is known 
+    
+    # We will do a BFS walk using a the squares queue.
+    # This will guarantee that for every int n >= 0,
+    #   all paths of n moves are explored before
+    #   any path of n+1 moves is explored.
+    # This means that the first path that we encounter
+    # that land us on the destination square, dstSquare,
+    # is guaranteed to be a shortest path.
+    
+    while squares:
+        
+        square = squares.popleft()
+        min_num_moves_to_square = minNumMoves[square]
+        
+        for delta in MOVE_DELTAS:
+            newSquare = _process(square, delta, minNumMoves, min_num_moves_to_square, squares, dstSquare)
+            if newSquare == dstSquare:
+                return minNumMoves[newSquare]
+    
+    raise ValueError("Inputs src and dst must each be in range(0, {0})".format(NUM_ROWS*NUM_COLS))
+
+# In the future, decompose this helper function into smaller functions.
+# As it is, this function does a lot!!!
+def _process(square, delta, minNumMoves, min_num_moves_to_square, squares, dstSquare):
+    
+    newSquare = square.moveBy(delta)
+    
+    if newSquare.isInBoundsOf(board = minNumMoves) and minNumMoves[newSquare] is None:
+        
+        min_num_moves_to_newSquare = min_num_moves_to_square + 1
+        minNumMoves[newSquare]     = min_num_moves_to_newSquare
+        squares.append(newSquare)
+    
+    return newSquare
+
 class Square(object):
     
     def __init__(self, row, col):
@@ -26,11 +84,12 @@ class Square(object):
     @classmethod
     def fromNum(cls, num, board):
         num_cols = board.getNumCols()
-        row = num / NUM_COLS
-        col = num % NUM_COLS
+        row = num / num_cols
+        col = num % num_cols
         return cls(row, col)
     
     def __eq__(self, other):
+        
         if not isinstance(other, self.__class__):
             return False
         
@@ -39,7 +98,7 @@ class Square(object):
     def getRowColPair(self):
         return (self.row, self.col)
     
-    # returns new square, does not modify old one
+    # returns new square, does not self
     def moveBy(self, delta):
         ROW, COL = 0, 1 # indeces into the delta tuple
         new_row = self.row + delta[ROW]
@@ -73,58 +132,6 @@ class Board(object):
     def __setitem__(self, square, val):
         r, c = square.getRowColPair()
         self.board[r][c] = val
-
-from collections import deque
-
-''' PRECONDITION: src and dst are each in range(0, NUM_ROWS*NUM_COLS) '''
-def answer(src, dst):
-    
-    if src == dst:
-        return 0
-    
-    minNumMoves = Board(NUM_ROWS, NUM_COLS, None)
-    # for square sq with row == r and col == c,
-    # minNumMoves[sq] == minNumMoves.board[r][c] == the min num of moves to get
-    # from the source square, srcSquare,
-    # to sq the square with coords (r, c) if that value is known
-    #
-    # while that value is unknown, minNumMoves[sq] == None
-    
-    srcSquare = Square.fromNum(src, board = minNumMoves)
-    dstSquare = Square.fromNum(dst, board = minNumMoves)
-    
-    minNumMoves[srcSquare] = 0
-    
-    squares = deque([srcSquare]) # squares queue, notice that the min num of moves is known for each square in squares
-    
-    # we will do a BFS walk keeping track of the number of moves made so far
-    
-    while squares:
-        
-        square = squares.popleft()
-        min_num_moves_to_square = minNumMoves[square]
-        
-        for delta in MOVE_DELTAS:
-            newSquare = _process(square, delta, minNumMoves, min_num_moves_to_square, squares, dstSquare)
-            if newSquare == dstSquare:
-                return minNumMoves[newSquare]
-    
-    raise ValueError("input dst must be in range(0, {0})".format(NUM_ROWS*NUM_COLS))
-
-# in the future, decompose this helper function into smaller functions
-# as it is this function does a lot!!!
-def _process(square, delta, minNumMoves, min_num_moves_to_square, squares, dstSquare):
-    
-    newSquare = square.moveBy(delta)
-    
-    if newSquare.isInBoundsOf(board = minNumMoves) and minNumMoves[newSquare] is None:
-        
-        min_num_moves_to_newSquare = min_num_moves_to_square + 1
-        minNumMoves[newSquare]     = min_num_moves_to_newSquare
-        squares.append(newSquare)
-    
-    return newSquare
-
 
 # TESTS
 
